42f802e0812f3d2483949acefa933278ee1c9dd7
marmai-jma <johane.marmai@gmail.com>
2019-11-15 14:41:17 +0100

    Version avec GildedRoseApprovalTests et des fichiers approved renommé depuis les fichiers received produits

--- src/test/java/com/gildedrose/GildedRoseApprovalTests.java
+++ src/test/java/com/gildedrose/GildedRoseApprovalTests.java
@@ -10,6 +10,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import java.io.ByteArrayOutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintStream;
+import java.util.Arrays;
 
 @ExtendWith(MockitoExtension.class)
 public class GildedRoseApprovalTests {
@@ -18,7 +19,7 @@ public class GildedRoseApprovalTests {
 
     @BeforeEach
     public void setUp() {
-        out = System.out;
+        out = System.out;    // remplacer la sortie console par ecriture dans un fichier de log
         System.setOut(new PrintStream(output));
     }
 
@@ -34,11 +35,63 @@ public class GildedRoseApprovalTests {
         Approvals.verify(output.toString());
     }
 
+
     @Test
-    void approve_operations_on_60_days() {
-        PrintStream out = System.out;
-        TexttestFixture.main(new String[]{"60"});
-        System.out.flush();
-        Approvals.verify(output.toString());
+    void default_product() {
+        Item[] items = new Item[]{
+                new Item("default",2,50),
+                new Item ("default",2,0),
+                new Item("default",2,10),
+                new Item ("default",0,10),
+                new Item("default",-1,10),
+        };
+        new GildedRose(items).updateQuality();
+        Approvals.verify(Arrays.deepToString(items));
+    }
+
+    @Test
+    void backstagepasses_to_a_TAFKAL80ETC_concert() {
+        Item[] items = new Item[]{
+                new Item("Backstage passes to a TAFKAL80ETC concert",2,50),
+                new Item("Backstage passes to a TAFKAL80ETC concert",2,40),
+                new Item ("Backstage passes to a TAFKAL80ETC concert",2,0),
+                new Item("Backstage passes to a TAFKAL80ETC concert",2,10),
+                new Item("Backstage passes to a TAFKAL80ETC concert",1,10),
+                new Item ("Backstage passes to a TAFKAL80ETC concert",0,10),
+                new Item("Backstage passes to a TAFKAL80ETC concert",-1,10),
+        };
+        new GildedRose(items).updateQuality();
+        Approvals.verify(Arrays.deepToString(items));
+    }
+
+    @Test
+    void aged_brie() {
+        Item[] items = new Item[]{
+                new Item("Aged Brie",2,50),
+                new Item("Aged Brie",2,40),
+                new Item ("Aged Brie",2,0),
+                new Item("Aged Brie",2,10),
+                new Item("Aged Brie",1,10),
+                new Item ("Aged Brie",0,10),
+                new Item("Aged Brie",-1,10),
+        };
+        new GildedRose(items).updateQuality();
+        Approvals.verify(Arrays.deepToString(items));
+    }
+
+    @Test
+    void sulfuras_hand_of_ragnaros() {
+        Item[] items = new Item[]{
+                new Item("Sulfuras, Hand of Ragnaros",100,50),
+                new Item("Sulfuras, Hand of Ragnaros",2,40),
+                new Item ("Sulfuras, Hand of Ragnaros",2,0),
+                new Item("Sulfuras, Hand of Ragnaros",2,10),
+                new Item("Sulfuras, Hand of Ragnaros",1,10),
+                new Item ("Sulfuras, Hand of Ragnaros",0,10),
+                new Item("Sulfuras, Hand of Ragnaros",-1,10),
+                new Item("Sulfuras, Hand of Ragnaros",-1,-1)
+        };
+        new GildedRose(items).updateQuality();
+        Approvals.verify(Arrays.deepToString(items));
     }
 }
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,5 +1,6 @@
 package com.gildedrose;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -7,11 +8,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void default_product_without_quality_at_sellIng_day() {
+        Item item = new Item("foo", 0, 0);
+        Item[] items = new Item[] { item };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        Assertions.assertAll(
+                () ->  assertEquals(-1, item.sellIn, "sellIn"),
+                () ->  assertEquals(0, item.quality, "quality")
+                );
     }
 
+    @Test
+    public void default_product_before__sellIn_quality_lowered_by_one() {
+        Item item = new Item("foo", 1, 2);
+        Item[] items = new Item[] { item };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        Assertions.assertAll(
+                () ->  assertEquals(0, item.sellIn, "sellIn"),
+                () ->  assertEquals(1, item.quality, "quality")
+        );
+    }
 }
